41efd43e3917ae0f3f156689de36499e48add39f
abhishek <abhishekdwaghmare2000@gmail.com>
2021-04-13 15:27:12 +0530

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const Navbar = ({ totalCounters }) =>{
+
+    return (
+        <nav className='navbar navbar-light bg-light'>
+            <a className='navbar-brand' href="https://www.google.com">Navbar{' '}
+            <span className='badge badge-pill badge-secondary'>
+                {totalCounters}
+            </span>
+            </a>
+        </nav>
+      );
+
+}
+ 
+export default Navbar;
